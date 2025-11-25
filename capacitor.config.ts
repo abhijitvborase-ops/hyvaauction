126b2b6c9852c556ae663e4f2d1abe8cde198baa
abhijitvborase-ops <abhijitvborase@gmail.com>
2025-11-25 15:45:38 +0000

    Angular build + capacitor setup + firebase fix

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hyvaauction.app',
+  appName: 'hyva-premier-league-2026',
+  webDir: 'dist'
+};
+
+export default config;
